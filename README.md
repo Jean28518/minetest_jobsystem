195f2f987d6f5acf65af88b7d83c709e377cef4c
Jean <martin.zie@gmx.de>
2019-03-06 15:56:46 +0100

    Adding functionaltiy

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-minetest_jobsystem
+# Minetest-Mod: jobsystem
+Github: https://github.com/Jean28518/minetest_jobsystem
+
+This mod adds a jobsystem to your server. You can only have one job on the same time.
+You can change your job after 10 hours servertime. That can be changed (see below).
+
+## For what can I use this mod?
+To handle the jobs of the players. On my server I use this mod in combination with adminshop, which supports licenses (jobsystem is based on licenses). So a player in a specific job can sell/buy some items to better prices for example.
+
+
+## How to use:
+`job` Display help
+
+`job info` Display your current job
+
+`job acquire <job_name>`
+
+
+### Pre Configured Jobs:
+`miner`, `farmer`,`hunter`, `builder`
+
+## How to configure:
+in `init.lua` you can change the countdown-time (default is 10 hours (36000 seconds))
+by changing the value of `DINGSTIME`. You can set it to zero if you want to disable
+the mecanism.
+
+You could also add custom jobs by adding your job to the sections. `-- Add here job:`.
+*(Dont forget to unassign your new job on every other case, because with this mod a player should have at least only one job assigned)*
---
+++ init.lua
@@ -0,0 +1,71 @@
+-- Add here job:
+licenses_add("miner")
+licenses_add("farmer")
+licenses_add("hunter")
+licenses_add("builder")
+
+-- Change here Countdown:
+DINGSTIME = 36000
+
+minetest.register_chatcommand("job", {
+  privs = {
+      interact = true,
+  },
+
+  func = function(player, param)
+    local mode, job = param:match('^(%S+)%s(.+)$')
+    local pmeta = minetest.get_player_by_name(player):get_meta()
+    local aqtime = pmeta:get_int("job:aqtime")
+    if  aqtime == nil then
+      aqtime = 0
+    end
+    if job == " acquire" then
+      minetest.chat_send_player(player, "You need to specify a job!")
+      return
+    end
+    if mode == "acquire" then
+      if minetest.get_gametime() - aqtime < DINGSTIME then
+        minetest.chat_send_player(player, "You can only change your job every " .. DINGSTIME .. " seconds!")
+      else
+        local changed = true
+        --- Add here job:
+        if job == "miner" then
+          licenses_unassign(player, "farmer")
+          licenses_unassign(player, "hunter")
+          licenses_unassign(player, "builder")
+        elseif job == "farmer" then
+          licenses_unassign(player, "miner")
+          licenses_unassign(player, "hunter")
+          licenses_unassign(player, "builder")
+        elseif job == "hunter" then
+          licenses_unassign(player, "miner")
+          licenses_unassign(player, "farmer")
+          licenses_unassign(player, "builder")
+        elseif job == "builder" then
+          licenses_unassign(player, "miner")
+          licenses_unassign(player, "farmer")
+          licenses_unassign(player, "hunter")
+        else
+          minetest.chat_send_player(player, "Job not known")
+          changed = false
+        end
+        if changed then
+          licenses_assign(player, job)
+          minetest.chat_send_player(player, "You are "..job.." now")
+          aqtime= minetest.get_gametime()
+          pmeta:set_int("job:aqtime", aqtime)
+          pmeta:set_string("job:job", job)
+        end
+      end
+    end
+    if param == "info" then
+      minetest.chat_send_player(player, "You are "..pmeta:get_string("job:job"))
+    elseif mode ~= "acquire" and job ~= " acquire" then
+      minetest.chat_send_player(player, "To acquire an job: " ..
+      "\njob acquire: <job_name>" ..
+      -- Add here job:
+      "\njobs: miner, farmer, hunter, builder")
+
+    end
+  end
+})
